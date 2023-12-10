de8ebc3dee07f3281c4cc2a899d7d75f129ce65d
Peter <peterfayez1285@gmail.com>
2023-12-10 22:31:59 +0200

    added other url  patterns for update/list brand, and function to delete brand

--- groceryshop/dashboard/brand/urls.py
+++ groceryshop/dashboard/brand/urls.py
@@ -7,5 +7,8 @@ app_name = 'brand_dashboard'
 
 urlpatterns = [
     path('create/', user_dashboard_permission(views.CreateBrand.as_view()), name = 'create_brand'),
-    
+    path('update/<int:pk>', user_dashboard_permission(views.UpdateView.as_view()), name = "update_brand"),
+    path('list/', views.ListBrand.as_view(), name="list_brand"),
+    path('delete/<int:pk>', user_dashboard_permission(views.delete_brand),
+         name= 'delete_brand'),
 ]
