21e268ea02f4fc640d4f1ee9a73035600fcd7f09
Ziad <Ziad.Hesham@ejust.edu.eg>
2023-12-23 18:20:17 +0200

    commit temp

--- groceryshop/coupons/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import PercentCoupon, VoucherCoupon
-
-# Register your models here.
-admin.site.register(PercentCoupon)
-admin.site.register(VoucherCoupon)
--- groceryshop/coupons/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import coupons_list
-
-app_name = "coupons"
-
-urlpatterns = [
-    path('', coupons_list, name="coupons_list"),
-]
