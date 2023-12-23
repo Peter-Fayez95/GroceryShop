71e9e065698679ecf37575ffe6880977575c579e
Peter <peterfayez1285@gmail.com>
2023-12-24 00:24:33 +0200

    push edits on data.py amd build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ python manage.py migrate
 python manage.py shell
 from django.contrib.auth.models import User
 User.objects.create_superuser(username='mongy', password='f3b4a44410a4d783b4cfa65c69a41a57', email='mongy@example.com')
-
+exit()
 python manage.py runscript data
