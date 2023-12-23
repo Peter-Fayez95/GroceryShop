53e8589667846590807f176a267f990d9eddb5e4
Peter <peterfayez1285@gmail.com>
2023-12-24 00:09:45 +0200

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,8 @@ set -o errexit
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-winpty python manage.py createsuperuser
-mongy
-mongy@example.com
-f3b4a44410a4d783b4cfa65c69a41a57
-f3b4a44410a4d783b4cfa65c69a41a57
+python manage.py shell
+from django.contrib.auth.models import User
+User.objects.create_superuser(username='mongy', password='f3b4a44410a4d783b4cfa65c69a41a57', email='mongy@example.com')
 
 python manage.py runscript data
