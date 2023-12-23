8370d24a800e14f91e55a494bd4c56e62f2c22e6
Peter <peterfayez1285@gmail.com>
2023-12-24 00:03:42 +0200

    Edited shell commands

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser
+winpty python manage.py createsuperuser
 mongy
 mongy@example.com
 f3b4a44410a4d783b4cfa65c69a41a57
